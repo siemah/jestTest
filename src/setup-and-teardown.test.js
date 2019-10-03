e3fc6e0286b6382fdf292c3fcc61e59fe8be482c
siemah <m_hamiche@inptic.edu.dz>
2019-10-03 18:21:36 +0100

    tips for shared state between test

--- src/setup-and-teardown.test.js
+++ src/setup-and-teardown.test.js
@@ -27,12 +27,21 @@ const initJobs =  () => new Promise((res, rej) => {
  * 1/- to verify if the test passed beside others
  *    just use only commade like line 31 test.only
  *    to skip all other tests just & run that test with only flag
+ * 2/- in many test one test fail & when run alone it passe
+ *     then we must see other test who interfer with this test
+ * 3/_ or use beforeEeach to fix shared state or to log data
  */
 
-test.only('should truthy', () => {
+test('should truthy', () => {
+  console.log('test will fail')
   expect(null).toBeTruthy();
 });
 
 test('should falsy', () => {
+  console.log('test will pass')
   expect(null).toBeFalsy();
 });
+
+beforeEach(() => {
+  console.log('clear shared state or log data')
+})
