5c07077c3e6390ff52579dafd550a07062a69220
siemah <m_hamiche@inptic.edu.dz>
2019-10-03 17:51:45 +0100

    order of test matter in describe blocks

--- src/setup-and-teardown.test.js
+++ src/setup-and-teardown.test.js
@@ -23,38 +23,34 @@ const initJobs =  () => new Promise((res, rej) => {
 });
 
 /**
- * SCOPE
- * the initializeDB function run for every test 
- * because its in global scope of all tests
+ * Order of execution of describe and other tests
+ * inside the describe block the order matter because
+ * it respect the order 
  */
-
-beforeEach(() => {
-  console.log('global before')
-  return initializeBD().then(res => {
-    _db = res;
+// describe('global describe', () => {
+  
+describe('descire Q', () => {
+  console.log('inner describe Q')
+  test('should inner describe Q', () => {
+    console.log('innet test 1 of describe Q');
+    expect(1).toBeTruthy();
+  });
+});
+console.log('between describe Q and test W')
+test('should inner test W', () => {
+  console.log('inner test W without descibe');
+  expect(1).toBeTruthy();
+});
+console.log('between describe C and test W')
+describe('describe inner C', () => {
+  console.log('describe inner C');
+  test('test for describe inner C', () => {
+    console.log('test for describe inner C');
+    expect(false).toEqual(false);
   });
 });
 
-test('should master "php"', () => {
-  expect(_db).toContain('php');
-});
-
-describe('mathing jobs to langs', () => {
-
-  let _jobs = [];
-  beforeEach( () =>{
-    console.log('describe before')
-    return initJobs().then(j => _jobs = j);
-  });
-
-  afterEach(() => {
-    _jobs = null;
-  });
+console.log('describe outer-c');
 
 
-  test('should be backend to master "php"', () => {
-    expect(_jobs).toContain('backend')
-  });
-
-})
-
+// })
