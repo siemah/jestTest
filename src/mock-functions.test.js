20096765115f909da40499f9b6a62c8fa992f4a1
siemah <m_hamiche@inptic.edu.dz>
2019-10-06 11:16:16 +0100

    mock propreties

--- src/mock-functions.test.js
+++ src/mock-functions.test.js
@@ -29,12 +29,27 @@ test('should mock forEach function', () => {
 
   // test if the mock function called twice see line 4(capturing params passed)
   // and will expect to be 4 & 3 in order
-  expect(forEachCallbackMock.mock.calls[0][0]).toBe(4);
-  expect(forEachCallbackMock.mock.calls[1][0]).toBe(3);
+  expect(forEachCallbackMock.mock.calls[0][0]).toBe(4);// 1st arg for 1st call
+  expect(forEachCallbackMock.mock.calls[1][0]).toBe(3);// 1st arg for 2st call
+  // expect(forEachCallbackMock.mock.calls[1][1]).toBe(3);// 2st arg for 2st call
 
   // test the return value/s see line 5
-  expect(forEachCallbackMock.mock.results[0].value).toBe(5);
-  expect(forEachCallbackMock.mock.results[1].value).toBe(4);
+  expect(forEachCallbackMock.mock.results[0].value).toBe(5);// result of 1st call
+  expect(forEachCallbackMock.mock.results[1].value).toBe(4);// result of 2nd call
+
+  // how many this function instancied
+  expect(forEachCallbackMock.mock.instances.length).toBe(2);
+
+  // get the name of instance
+  // console.log(forEachCallbackMock.mock.instances[0].name)
 
 });
 
+// here will find how to capture instanciation of function
+const _mock = jest.fn();
+
+let a = new _mock();
+let b = {};
+let bound = _mock.bind(b);
+bound()
+console.log(_mock.mock.instances) // where mockConstructor {} is a & {} is b 
