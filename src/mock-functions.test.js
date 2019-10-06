ff20bdbe2ea9a4a2f41213fb06ebfb520efd1b07
siemah <m_hamiche@inptic.edu.dz>
2019-10-06 10:58:50 +0100

    use mock function by creating a mock

---
+++ src/mock-functions.test.js
@@ -0,0 +1,40 @@
+/**
+ * mock a function 
+ * 1- to see if will erasing its implementation
+ * 2- capturing calls to this function & params passed in those calls
+ * 3- test return values
+ * 4- capturing instanciation of this function
+ * there is 2 ways to mock a function: 
+ * 1/- create a mock to test it
+ * 2/- writing a manual mock to override a module dependency.
+ */
+
+// USING MOCK FUNCTION
+// forEach is a function to mock
+function forEach(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index]);
+  }
+}
+test('should mock forEach function', () => {
+
+  // create a forEach mock function
+  const forEachCallbackMock = jest.fn(item => item + 1);
+
+  // use a mock function with forEach
+  forEach([4, 3], forEachCallbackMock);
+
+  // test if the mock function called twice see line 4(capturing calls)
+  expect(forEachCallbackMock.mock.calls.length).toBe(2);
+
+  // test if the mock function called twice see line 4(capturing params passed)
+  // and will expect to be 4 & 3 in order
+  expect(forEachCallbackMock.mock.calls[0][0]).toBe(4);
+  expect(forEachCallbackMock.mock.calls[1][0]).toBe(3);
+
+  // test the return value/s see line 5
+  expect(forEachCallbackMock.mock.results[0].value).toBe(5);
+  expect(forEachCallbackMock.mock.results[1].value).toBe(4);
+
+});
+
