6ef99d69517d5b49533940281e1fe667213e33dc
siemah <m_hamiche@inptic.edu.dz>
2019-10-26 12:03:45 +0100

    complexe behavior mocking

--- src/mock-functions.test.js
+++ src/mock-functions.test.js
@@ -1,7 +1,11 @@
-jest.mock('./modules/foo');
-const foo = require('./modules/foo');
+let mockfn = jest.fn()
+  .mockImplementationOnce(cb => cb(null, true))
+  .mockImplementationOnce(cb => cb(null, false))
 
-test('should mock function implementation', () => {
-  foo.mockImplementation(n => n);
-  console.log(foo())
+test('should mock ', () => {
+
+  mockfn((err, val) => console.log(val));
+  // > true
+
+  mockfn((err, val) => console.log(val));
 })
