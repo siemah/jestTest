d6eff3a00efe1d1dc742049836e35598ad261871
siemah <m_hamiche@inptic.edu.dz>
2019-11-03 11:46:32 +0100

    using async/await lonely and with resolves

--- src/__tests__/user-test.js
+++ src/__tests__/user-test.js
@@ -14,5 +14,20 @@ it('expected to work with promises', () => {
 it('work with resolves', () => {
   expect.assertions(1);
   return expect(user.getUserName(5)).resolves.toEqual('Paul')
-}
-)
+})
+
+it('work with async/await', async () => {
+  expect.assertions(1);
+  let name = await user.getUserName(5);
+  expect(name).toEqual('Paul');
+});
+
+it('work with async/await and resolves together 1', async () => {
+  expect.assertions(1);
+  await expect(user.getUserName(4)).resolves.toEqual('Mark');
+});
+
+it('work with async/await and resolves together 2', async () => {
+  expect.assertions(1);
+  expect(await user.getUserName(5)).toEqual('Paul')
+});
